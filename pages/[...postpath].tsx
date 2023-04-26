f1881e041623b47dca514c08ed280596ac853d48
amazingmedia <131604200+amazingmedia@users.noreply.github.com>
2023-04-26 15:39:43 +0630

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -102,7 +102,7 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title>{post.title}</title>
+				<title>{PostTypeSEO.opengraphTitle}</title>
 			</Head>
 			<div className="post-container">
 				<h1>{post.title}</h1>
